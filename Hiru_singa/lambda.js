89b712813ba244d68c14326cf8898a7ac8c663f4
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-08 15:00:40 +0530

    new DB

--- Hiru_singa/lambda.js
+++ Hiru_singa/lambda.js
@@ -1,6 +1,20 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
 
+	ddb.put({
+		TableName: 'HiruTable',
+		Item: { 'ID': '002', 'Name': 'Hiru' }
+	}, function (err, data) {
+		if (err) {
+			console.log('error : ', err);
+			callback(null, err);
+		} else {
+			//your logic goes here
+			console.log('data : ', data);
+			callback(null, 'Successfully executed');
+		}
+	});
 
-	callback(null,'Successfully executed');
+	//callback(null, 'Successfully executed');
 }
