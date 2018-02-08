cd379e04b51cbde98c84904b51dc82a2bbfa5638
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-08 15:24:56 +0530

    db scan

---
+++ Hiru_singa/lambda02.js
@@ -0,0 +1,26 @@
+let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
+
+	ddb.scan({
+		TableName: 'HiruTable',
+		ExpressionAttributeValues: {
+			':name': 'Hiru'
+		},
+		FilterExpression: 'Name = :name'
+	}, function (err, data) {
+		if (err) {
+			//handle error
+			console.log('error : ', err);
+			callback(null, err);
+		} else {
+			//your logic goes here
+			console.log('data : ', data);
+			callback(null, 'Successfully executed');
+		}
+	});
+
+
+
+	callback(null, 'Successfully executed');
+}
